73e15b395b051df13c870d1d41ba4110f447fc07
onsel1974 <onsel1974@gmail.com>
2026-08-01 15:26:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -86,6 +86,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
         }
 
+
+   if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./service-worker.js")
+      .then(registration => {
+        console.log(
+          "Service worker registered with scope:",
+          registration.scope
+        );
+      })
+      .catch(error => {
+        console.error(
+          "Service worker registration failed:",
+          error
+        );
+      });
+  });
+}    
+
     }
 
 });
